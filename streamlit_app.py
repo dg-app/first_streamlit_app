303822616f417f79168527012c229f01b77343b9
dg-app <126774096+dg-app@users.noreply.github.com>
2023-03-04 23:36:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,11 +52,7 @@ def get_fruit_load_list():
 
    
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
-#my_data_rows = my_cur.fetchall()
 my_data_rows = get_fruit_load_list()
-#streamlit.header('The fruit load list contains:')
 streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
