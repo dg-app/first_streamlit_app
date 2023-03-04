f77a712e80ed82a65e0b64a650ea734da5299d05
dg-app <126774096+dg-app@users.noreply.github.com>
2023-03-04 23:24:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if streamlit.button('Get Friut Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
-   streamlit.stop()
+ #  streamlit.stop()
    
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
